7afe1811e180ddba0cf673f3827ba2f77262bb38
Macleod Robert <sarca571ca@gmail.com>
2023-05-25 19:02:04 +0800

    Removed tokens from file

--- bottoken.py
+++ bottoken.py
@@ -1,2 +1,2 @@
-wd_tod = 'MTExMDM3NjUyNjgwMjg1Mzk3OQ.GxNguG.7r2bQPC8w-USwXEb0VCUKS-gk85266IBF-XcWM'
-wd_dkp_bot = 'MTEwODY1MTQxMDI4NDg4ODA2NA.GfstjG.Kdu97sk1ktv43ukZ82NmnrCsnrI0nL46HqQNos'
+wd_tod = ''
+wd_dkp_bot = ''
