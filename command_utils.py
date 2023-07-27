a1d6964a21e3255825f10f1b2fc811ea01b54d2a
sarca571ca <sarca571ca@gmail.com>
2023-07-27 01:48:23 -0700

    Minor bug fix.

--- command_utils.py
+++ command_utils.py
@@ -173,7 +173,8 @@ async def process_hnm_window(hnm_window_end, target_time, hnm, hnm_time, date,
                         if task.get_name() == f"wm-{channel_name}":
                             break
                     else:
-                        await restart_channel_tasks(guild, channel_name, category, time_diff, existing_channel)
+                        if str(existing_channel.category) == hnm_att_category_name:
+                            await restart_channel_tasks(guild, channel_name, category, time_diff, existing_channel)
                 else:
                     channel_name = f"{channel_name}1"
                     existing_channel = discord.utils.get(guild.channels, name=channel_name)
