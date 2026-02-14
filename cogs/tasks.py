6a0bc1d032c8081edf9051f4e41e33545a350bb4
Robert MacLeod <sarca571ca@gmail.com>
2026-02-14 17:30:14 -0500

    Added limit to camp pings channel

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -1,17 +1,19 @@
-import discord
 import asyncio
 
-from discord.ext import tasks, commands
-from src import hnmutil, timeutil, stringutil, channelutil, settings
+import discord
+from discord.ext import commands, tasks
+
+from src import channelutil, hnmutil, settings, stringutil, timeutil
 
 log_print = stringutil.StringUtil.log_print
+
 
 class SendHourWarning(commands.Cog):
 
     def __init__(self, bot) -> None:
         self.send_hour_warning.start()
         self.bot = bot
-    
+
     def cog_unload(self) -> tasks.Coroutine[tasks.Any, tasks.Any, None]:
         self.send_hour_warning.cancel()
 
@@ -22,7 +24,9 @@ class SendHourWarning(commands.Cog):
         camp_pings_channel = self.bot.get_channel(settings.CAMPPINGS)
         now = timeutil.Time.now()
         try:
-            async for message in hnm_timers_channel.history(limit=None, oldest_first=True):
+            async for message in hnm_timers_channel.history(
+                limit=None, oldest_first=True
+            ):
                 if message.content.startswith("- "):
                     timestamp, dt = timeutil.Time.strip_timestamp(message)
                     warning_msg = message.content.replace("- ", "", 1)
@@ -31,24 +35,36 @@ class SendHourWarning(commands.Cog):
 
                     if camp_end_time >= now and hour_warn_time <= now:
                         if camp_pings_channel:
-                            last_message = [msg async for msg in camp_pings_channel.history(limit=1)]
+                            last_message = [
+                                msg async for msg in camp_pings_channel.history(limit=1)
+                            ]
 
                             if last_message:
                                 message_sent = False
 
-                                async for msg in camp_pings_channel.history(limit=None, oldest_first=False):
-                                    if warning_msg == msg.content.replace("@everyone ", "", 1):
+                                async for msg in camp_pings_channel.history(
+                                    limit=100, oldest_first=False
+                                ):
+                                    if warning_msg == msg.content.replace(
+                                        "@everyone ", "", 1
+                                    ):
                                         message_sent = True
                                         break
 
                                 if not message_sent:
-                                    await camp_pings_channel.send(f"@everyone {warning_msg}")
+                                    await camp_pings_channel.send(
+                                        f"@everyone {warning_msg}"
+                                    )
                             else:
-                                await camp_pings_channel.send(f"@everyone {warning_msg}")
+                                await camp_pings_channel.send(
+                                    f"@everyone {warning_msg}"
+                                )
                 pass
         except discord.errors.DiscordServerError as e:
             if e.code == 0 and e.status == 503:
-                msg = log_print("SendHourWarning: Service Unavailable error. Retrying in 60 seconds...")
+                msg = log_print(
+                    "SendHourWarning: Service Unavailable error. Retrying in 60 seconds..."
+                )
                 await channelutil.LogPrint.print(self.bot, msg)
                 await asyncio.sleep(60)
                 SendHourWarning.send_hour_warning.restart()
@@ -58,6 +74,7 @@ class SendHourWarning(commands.Cog):
         except Exception as e:
             msg = log_print(f"SendHourWarning: Error -> {e}")
             await channelutil.LogPrint.print(self.bot, msg)
+
 
 class CreateChannelTasks(commands.Cog):
 
@@ -75,20 +92,24 @@ class CreateChannelTasks(commands.Cog):
         hnm_times_channel = self.bot.get_channel(settings.HNMTIMES)
 
         if not category:
-            msg = log_print("HNM Attendance: Catergory either not set properly in settings or doesn't exist.")
+            msg = log_print(
+                "HNM Attendance: Catergory either not set properly in settings or doesn't exist."
+            )
             await channelutil.LogPrint.print(self.bot, msg)
 
         now = timeutil.Time.now()
         target_time = timeutil.Time.format_time(now)
         try:
-            async for message in hnm_times_channel.history(limit=None, oldest_first=True):
+            async for message in hnm_times_channel.history(
+                limit=None, oldest_first=True
+            ):
                 if message.content.startswith("- "):
 
                     # Extract the day
                     day_start = message.content.find("(")
                     day_end = message.content.find(")")
                     if day_start != -1 and day_end != -1:
-                        raw_day = message.content[day_start + 1:day_end].strip()
+                        raw_day = message.content[day_start + 1 : day_end].strip()
                         day = stringutil.StringUtil.remove_formating(raw_day)
                     else:
                         day = None
@@ -106,7 +127,9 @@ class CreateChannelTasks(commands.Cog):
                             nq = message.content[4:7].strip()
                             channel_name = f"{nq}"
 
-                    timestamp, dt = timeutil.Time.strip_timestamp(message)# dt, utc = calculate_time_diff(message.content) # Extracts the utc timestamp
+                    timestamp, dt = timeutil.Time.strip_timestamp(
+                        message
+                    )  # dt, utc = calculate_time_diff(message.content) # Extracts the utc timestamp
                     dt_pst = dt.astimezone(timeutil.Time.bot_tz())
                     date = dt_pst.strftime("%b%d").lower()
                     hnm = channel_name.upper()
@@ -114,19 +137,42 @@ class CreateChannelTasks(commands.Cog):
                     unix_target = int(dt.timestamp())
                     time_diff = now - unix_target
 
-                    hnm_time = timeutil.Time.format_time(timestamp - (settings.MAKECHANNEL * 60))
-                    if any(keyword in channel_name.lower() for keyword in ["jor", "vrt", "tia"]):
-                        hnm_window_end = timeutil.Time.format_time(timestamp + (24 * 3600)) # 24 hours for GW
+                    hnm_time = timeutil.Time.format_time(
+                        timestamp - (settings.MAKECHANNEL * 60)
+                    )
+                    if any(
+                        keyword in channel_name.lower()
+                        for keyword in ["jor", "vrt", "tia"]
+                    ):
+                        hnm_window_end = timeutil.Time.format_time(
+                            timestamp + (24 * 3600)
+                        )  # 24 hours for GW
                     else:
-                        hnm_window_end = timeutil.Time.format_time(timestamp + (1 * 3600)) # 1 hour for everything else
+                        hnm_window_end = timeutil.Time.format_time(
+                            timestamp + (1 * 3600)
+                        )  # 1 hour for everything else
 
-                    await hnmutil.HandleHNM.process_hnm_window(hnm_window_end, target_time, hnm, hnm_time, date,
-                                             day, message, guild, category, timestamp, hnm_times_channel,
-                                             hnm_name, time_diff)
+                    await hnmutil.HandleHNM.process_hnm_window(
+                        hnm_window_end,
+                        target_time,
+                        hnm,
+                        hnm_time,
+                        date,
+                        day,
+                        message,
+                        guild,
+                        category,
+                        timestamp,
+                        hnm_times_channel,
+                        hnm_name,
+                        time_diff,
+                    )
                 pass
         except discord.errors.DiscordServerError as e:
             if e.code == 0 and e.status == 503:
-                msg = log_print("CreateChannelTask: Service Unavailable error. Retrying in 60 seconds...")
+                msg = log_print(
+                    "CreateChannelTask: Service Unavailable error. Retrying in 60 seconds..."
+                )
                 await channelutil.LogPrint.print(self.bot, msg)
                 await asyncio.sleep(60)
                 CreateChannelTasks.create_channel_task.restart()
@@ -136,6 +182,7 @@ class CreateChannelTasks(commands.Cog):
         except Exception as e:
             msg = log_print(f"CreateChannelTask: Error -> {e}")
             await channelutil.LogPrint.print(self.bot, msg)
+
 
 class DeleteOldChannels(commands.Cog):
 
@@ -149,12 +196,21 @@ class DeleteOldChannels(commands.Cog):
     @tasks.loop(hours=24)
     async def delete_old_channels(self):
         try:
-            archive_category = discord.utils.get(self.bot.guilds[0].categories, name=settings.ATTENDARCHID)
+            archive_category = discord.utils.get(
+                self.bot.guilds[0].categories, name=settings.ATTENDARCHID
+            )
             if archive_category:
                 for channel in archive_category.channels:
                     if isinstance(channel, discord.TextChannel):
-                        if (timeutil.Time.discord_tz() - channel.created_at).days >= settings.ARCHIVEWAIT:
-                            await channelutil.Manager.archive_channels(archive_category, channel, archive_category, option="delete")
+                        if (
+                            timeutil.Time.discord_tz() - channel.created_at
+                        ).days >= settings.ARCHIVEWAIT:
+                            await channelutil.Manager.archive_channels(
+                                archive_category,
+                                channel,
+                                archive_category,
+                                option="delete",
+                            )
             pass
         except discord.errors.DiscordServerError as e:
             if e.code == 0 and e.status == 503:
@@ -173,5 +229,6 @@ async def setup(bot: commands.Bot):
     await bot.add_cog(SendHourWarning(bot))
     await bot.add_cog(CreateChannelTasks(bot))
     await bot.add_cog(DeleteOldChannels(bot))
-    msg = log_print('All tasks loaded. Alise is online.')
+    msg = log_print("All tasks loaded. Alise is online.")
     await channelutil.LogPrint.print(bot, msg)
+
